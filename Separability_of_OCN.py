22ab786f491b0ac56be8a6ddde6b068055722a56
Aryan Mahajan <aryansahilm@gmail.com>
2025-01-09 19:20:43 +0530

    merge master

--- Separability_of_OCN.py
+++ Separability_of_OCN.py
@@ -1,10 +1,7 @@
 from sympy import gcd
 import numpy as np
 from scipy.optimize import linprog
-<<<<<<< HEAD
-=======
 from itertools import product
->>>>>>> master
 
 # Definition of One Counter Nets (Q, α0, αf, T; T=0) where T=0 is empty set
 class OneCounterNet:
@@ -101,45 +98,6 @@ def parse_ocn(file_path):
 
     return OneCounterNet(num_states, initial_state, final_state, transitions)
 
-<<<<<<< HEAD
-# Sample OCN for tests
-def get_sample_ocn():
-    test_ocn1 = (5,1,2,[[1,'a',2,1],[2,'b',1,-1]])
-    test_ocn2 = (3,1,2,[[1,'a',3,0],[2,'b',3,-2]])
-    return test_ocn1,test_ocn2
- 
-# Sample Linear Path Scheme for tests   
-def get_sample_path_scheme():
-    test_scheme = [
-        [
-            [1, 2, 3, 4, -2, 1],
-            [3, 4, 1, 2, -3, 4],
-            [2, 1, 4, 3, 2, -1]
-        ],
-        [
-            [4, 3, 2, 1, -3, 2],
-            [2, 3, 4, 1, 4, -3],
-            [1, 2, 3, 4, 1, -2]
-        ],
-        [
-            [3, 4, 2, 1, -4, 3],
-            [1, 3, 4, 2, 3, -1],
-            [4, 2, 1, 3, -2, 4]
-        ]
-    ]
-    return test_scheme
-
-# Sample Linear Set for tests
-def get_sample_linear_set():
-    b = 8
-    P = [
-        [1, 2, 3],
-        [3, 4, 1],
-        [2, 3, 4]
-    ]
-    return b,P
-=======
->>>>>>> master
 
 # Reachability in Two-Dimensional Vector Addition Systems with States (PSPACE)
 def ProduceLinPathScheme(p, q):
@@ -151,94 +109,6 @@ def ProduceLinPathScheme(p, q):
 def check_disjointness(ocn1,ocn2):
     return True
 
-<<<<<<< HEAD
-# Unit test for checking disjointness
-def test_disjointness():
-    test_ocn1,test_ocn2=get_sample_ocn()
-    assert check_disjointness(test_ocn1,test_ocn2)
-
-# Helper function for Calculate_profile. This computes net effects by the alpha/beta sequence
-# and minimum value of counter required to execute the sequence 
-def compute_effect_and_min_counters(sequence):
-    total_effect = [0, 0]  # Assuming counters are in 2D
-    min_values = [0, 0]  # Minimal values
-    
-    for t in sequence:
-        _, _,  _, _, counter1, counter2 = t
-        
-        # Update total effect
-        total_effect[0] += counter1
-        total_effect[1] += counter2
-        # Update minimum counters
-        min_values[0] = min(min_values[0], counter1)
-        min_values[1] = min(min_values[1], counter2)
-    
-    return total_effect, min_values
-
-# Returns 4k+2 integer pairs a,b,c,d given the Linear path schema
-def calculate_profile(path_scheme):
-    
-    profile = {"a": [], "c": [], "b": [], "d": []}
-    # a = Effects of alpha sequences
-    # c = Minimun counter values for alpha sequence
-    # b = Effects of beta sequences
-    # d = Minimum counter values for beta sequence
-    
-    # Iterate through the linear path scheme
-    index=0
-    for sequence in path_scheme:
-        effect, min_values = compute_effect_and_min_counters(sequence)
-        min_counters=[0,0]
-        min_counters[0] = max(0,0-min_values[0])
-        min_counters[1] = max(0,0-min_values[1])
-        if index%2 == 0:
-            profile["a"].append(effect)
-            profile["c"].append(min_counters)
-        elif index%2 == 1:
-            profile["b"].append(effect)
-            profile["d"].append(min_counters)
-        index=index+1
-    
-    return profile
-
-# Unit test for Calculating profile - (4k+2) integers
-def test_calculate_profile():
-    test_scheme = get_sample_path_scheme()
-    result = {'a': [[-3, 4], [-3, 6]], 'c': [[3, 1], [4, 1]], 'b': [[2, -3]], 'd': [[3, 3]]}
-    assert calculate_profile(test_scheme) == result
-    
-def effect_in_alpha_sequence(profile, x, y, i):
-    if x > profile['c'][i][0] and y > profile['c'][i][1]:
-        x += profile['a'][i][0]
-        y += profile['a'][i][1]
-    return x,y
-
-def effect_beta_sequence_in_first_quadrant(profile,i,x,y,bounds):
-    
-
-def calculate_linear_equations(profile, initial_counters, final_counters, bounds):
-    k = max(len(profile["a"]), len(profile["b"]))  # Get the maximum length among alpha and beta sequences
-    x = initial_counters[0]
-    y = initial_counters[1]
-    for i in range(k):
-        # Check and iterate over alpha sequences (a[i], c[i]) if they exist
-        if i < len(profile["a"]) and i < len(profile["c"]):
-            x,y = effect_in_alpha_sequence(profile, x, y, i)
-                
-        # Check and iterate over beta sequences (b[i], d[i]) if they exist
-        if i < len(profile["b"]) and i < len(profile["d"]):
-            if profile['a'][i][0] > x and profile['a'][i][1] > y:
-                effect_beta_sequence_in_first_quadrant(profile,i,x,y,bounds)
-            if profile['a'][i][0] < x and profile['a'][i][1] > y:
-                effect_beta_sequence_in_second_quadrant(profile,i,x,y,bounds)
-            if profile['a'][i][0] < x and profile['a'][i][1] < y:
-                effect_beta_sequence_in_third_quadrant(profile,i,x,y,bounds)
-            if profile['a'][i][0] > x and profile['a'][i][1] < y:
-                effect_beta_sequence_in_fourth_quadrant(profile,i,x,y,bounds)
-            
-def is_minimal(solution, solutions):
-    """Check if a solution is minimal in the set."""
-=======
 
 
 def effect_in_alpha_sequence(profile, x, y, i):
@@ -318,22 +188,11 @@ def calculate_linear_equations(profile, initial_counters, final_counters, bounds
             
 def is_minimal(solution, solutions):
     # Check if a solution is minimal in the set.
->>>>>>> master
     for s in solutions:
         if np.all(solution >= s) and np.any(solution > s):
             return False
     return True
 
-<<<<<<< HEAD
-def get_bounds_for_LPS(cross_product):
-    #Dummy return
-    length_of_LPS = 10
-    length_alpha = 10
-    length_beta = 10
-    return length_of_LPS,length_alpha,length_beta  
-
-
-=======
 # Lemma 22 :
 # Length of Linear Path Schema is bounded where bound is exponential
 # Lenght of loops is bounded where bound is polynomial
@@ -365,7 +224,6 @@ def enumerate_semi_linear_set(semi_linear_set):
         product.append((b, p))
 
     return product
->>>>>>> master
     
 # Condition a of Lemma 19 
 # Checks if first two coordinates are positive in any vector of P so that we get
@@ -381,13 +239,6 @@ def check_if_first_two_coordinates_can_grow(P):
             return True
     return False # P cannot be constructed. Therefore it does not contain n-witness
 
-<<<<<<< HEAD
-# Unit test
-def test_if_first_two_coordinates_can_grow():
-    b3,P = get_sample_linear_set()
-    assert check_if_first_two_coordinates_can_grow(P) == True
-=======
->>>>>>> master
 
 # Condition b of Lemma 19 using Proposition 20
 # Checks if gcd of third coordinates of vectors in P divides b3
@@ -401,14 +252,6 @@ def check_if_b3_is_a_linear_combination_of_P3(b3,P):
     
     return False  # g does not divide b3. Therefore it does not contain n-witness
 
-<<<<<<< HEAD
-# Unit test
-def test_if_b3_is_a_linear_combination_of_P3():
-    b3,P = get_sample_linear_set()
-    assert check_if_b3_is_a_linear_combination_of_P3(b3,P) == True
-
-=======
->>>>>>> master
 # Lemma 19 : Checks if the linear set L = b3 + P* contains n-witnesses for all n>0
 def check_n_witness(b3,P):
     return check_if_first_two_coordinates_can_grow(P) and check_if_b3_is_a_linear_combination_of_P3(b3,P)
@@ -426,13 +269,6 @@ def check_separability(ocn1,ocn2):
     # cross_product_automaton.display()                                 # Uncomment to display the states and transitions of the cross-product
     # cross_product_automaton.write_to_file("cross_product_output.txt") # Uncomment to display the cross-product in text file
 
-<<<<<<< HEAD
-    for p,q in cross_product_automaton.states:
-        Lin_Path_Schemes=ProduceLinPathScheme((0,0),(p,q))
-        for scheme in Lin_Path_Schemes:
-            profile = calculate_profile(scheme)
-            result = calculate_linear_equations(profile,[p,q])
-=======
     # For PREF (0,0)->(p,q)
     for p,q in cross_product_automaton.states:
         # Get the linear path schemes for PREF from (0,0) to (p,q)
@@ -463,7 +299,6 @@ def check_separability(ocn1,ocn2):
             semi_linear_set = calculate_semi_linear_set(result)
             enumerate_semi_linear_set(semi_linear_set)
     
->>>>>>> master
             
 
 if __name__ == "__main__":
@@ -476,11 +311,3 @@ if __name__ == "__main__":
         print("The One Counter Nets are separable")
     else:
         print("The One Counter Nets are not separable")
-<<<<<<< HEAD
-
-    # test_disjointness()
-    # test_calculate_profile()
-    # test_if_first_two_coordinates_can_grow()
-    # test_if_b3_is_a_linear_combination_of_P3()
-=======
->>>>>>> master
